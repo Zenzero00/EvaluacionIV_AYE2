572282cb2ca4462d9527e772b85b0d3a8c3d50aa
Francisco Unda <unda2001@gmail.com>
2024-06-18 10:55:48 -0400

    second commit: Actualizacion Menu Tareas

--- Gestion_de_proyectos.py
+++ Gestion_de_proyectos.py
@@ -21,3 +21,37 @@ class Proyecto:
         self.gerente = gerente
         self.equipo = equipo
         self.tarea = []
+
+    def agregar_tarea(self, tarea):
+        self.tarea.append(tarea)
+
+
+class Tarea:
+    def __init__(
+        self,
+        id,
+        nombre,
+        empresa_cliente,
+        descripcion,
+        fecha_inicio,
+        fecha_vencimiento,
+        estado,
+        porcentaje,
+    ):
+        self.id = id
+        self.nombre = nombre
+        self.empresa_cliente = empresa_cliente
+        self.descripcion = descripcion
+        self.fecha_inicio = fecha_inicio
+        self.fecha_vencimiento = fecha_vencimiento
+        self.estado = estado
+        self.porcentaje = porcentaje
+        self.subtareas = []
+
+
+class Subtarea:
+    def __init__(self, id, nombre, descripcion, estado):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.estado = estado
